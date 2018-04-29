e177272483c80e346a17889d5046d717cd035f46
Dade Sheng <dade@live.cn>
2018-04-29 00:00:35 -0400

    initial website

---
+++ js/config.js
@@ -0,0 +1,7 @@
+var configConstants = {
+    auth0: {
+        domain: 'shengdade.auth0.com',
+        clientId: 'gnN9JL8h26Mf4FxILjrSzvAeZUsqVhKz'
+    },
+    apiBaseUrl: 'https://26p4cwmpbe.execute-api.us-east-1.amazonaws.com/dev'
+};
